1a57d5ad8b98f74b68862e5eab9ae54ea9a59608
LovesAsuna <qq625924077@gmail.com>
2021-11-07 09:48:08 +0800

    :sparkles: feat(detector): SimplifyDetector

---
+++ src/main/kotlin/com/hyosakura/analyzer/grammar/detector/SimplifyDetector.kt
@@ -0,0 +1,100 @@
+package com.hyosakura.analyzer.grammar.detector
+
+import com.hyosakura.analyzer.grammar.Grammar
+import com.hyosakura.analyzer.grammar.NonTerm
+import com.hyosakura.analyzer.grammar.Symbol
+import com.hyosakura.analyzer.grammar.Term
+import java.util.*
+
+/**
+ * @author LovesAsuna
+ **/
+open class SimplifyDetector : Detector {
+    private val dfaPathMap: MutableMap<NonTerm, Boolean> = mutableMapOf()
+    private var hasCycle = false
+    private val pathStack: Deque<NonTerm> = LinkedList()
+
+    override fun detect(grammar: Grammar): Grammar {
+        removeRedundant(grammar)
+        // init pathMap
+        grammar.rules.keys.forEach {
+            dfaPathMap[it] = false
+        }
+        removeHarmful(grammar, grammar.head)
+        return grammar
+    }
+
+    private fun removeHarmful(grammar: Grammar, nonTerm: NonTerm) {
+        val value = grammar.rules[nonTerm]
+        if (dfaPathMap[nonTerm]!!) {
+            val set = mutableSetOf<NonTerm>()
+            if (pathStack.first == nonTerm) {
+                set.add(pathStack.first)
+            } else {
+                while (pathStack.first != nonTerm) {
+                    set.add(pathStack.pop())
+                }
+                set.add(pathStack.first)
+            }
+            if (set.stream().noneMatch { hasTerm(grammar.rules[it]!!) }) {
+                grammar.rules.keys.removeAll(set)
+                fun removeInvalidValue() {
+                    grammar.rules.values.forEach { outer ->
+                        outer.removeIf { inner ->
+                            inner.stream().anyMatch {
+                                set.contains(it) || (it is NonTerm && it !in grammar.rules.keys)
+                            }
+                        }
+                    }
+                }
+                fun removeEmptyValue() {
+                    grammar.rules.values.remove(grammar.rules.values.stream().filter {
+                        it.isEmpty()
+                    }.findFirst().orElseGet { null })
+                }
+                var size = 0
+                while (size != grammar.rules.size) {
+                    size = grammar.rules.size
+                    removeInvalidValue()
+                    removeEmptyValue()
+                }
+            }
+            set.clear()
+            return
+        }
+        dfaPathMap[nonTerm] = true
+        pathStack.push(nonTerm)
+        val flattenList = value!!.flatten()
+        for (element in flattenList) {
+            if (element is NonTerm) {
+                removeHarmful(grammar, element)
+                if (hasCycle) {
+                    return
+                }
+            }
+        }
+        dfaPathMap[nonTerm] = false
+    }
+
+    private fun hasTerm(list: List<List<Symbol>>): Boolean {
+        return list.stream().anyMatch {
+            it.size == 1 && it.first() is Term
+        }
+    }
+
+    private fun removeRedundant(grammar: Grammar) {
+        val left = grammar.rules.keys
+        val right = grammar.rules.values.flatMap { outer ->
+            outer.flatten().filterIsInstance<NonTerm>()
+        }
+        val redundantSet = mutableSetOf<NonTerm>().apply {
+            addAll(left)
+            addAll(right)
+            removeAll(left.toMutableSet().also { it.retainAll(right) })
+            remove(grammar.head)
+        }
+        redundantSet.forEach {
+            grammar.rules.remove(it)
+        }
+    }
+}
