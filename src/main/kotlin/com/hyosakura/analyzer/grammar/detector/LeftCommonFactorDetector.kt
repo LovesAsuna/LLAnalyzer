b526c7ba3ac5c3a93814a05ad0ba193b09ae472b
LovesAsuna <qq625924077@gmail.com>
2021-11-10 23:39:16 +0800

    :sparkles: feat(detector): LeftCommonFactorDetector

---
+++ src/main/kotlin/com/hyosakura/analyzer/grammar/detector/LeftCommonFactorDetector.kt
@@ -0,0 +1,45 @@
+package com.hyosakura.analyzer.grammar.detector
+
+import com.hyosakura.analyzer.grammar.Grammar
+import com.hyosakura.analyzer.grammar.NonTerm
+import com.hyosakura.analyzer.grammar.Symbol
+import com.hyosakura.analyzer.grammar.Term
+
+/**
+ * @author LovesAsuna
+ **/
+class LeftCommonFactorDetector : Detector {
+    override fun detect(grammar: Grammar): Grammar {
+        val iterator = grammar.rules.iterator()
+        val map = mutableMapOf<NonTerm, MutableList<MutableList<Symbol>>>()
+        while (iterator.hasNext()) {
+            val entry = iterator.next()
+            val key = entry.key
+            val value = entry.value
+            val firstTerm = value.first().first()
+            if (firstTerm is Term) {
+                if (
+                    value.size > 1 && value.stream().allMatch {
+                        it.first() == firstTerm
+                    }
+                ) {
+                    val newValue = mutableListOf<MutableList<Symbol>>()
+                    val newTerm = NonTerm("${key.symbol}'")
+                    newValue.add(mutableListOf(firstTerm, newTerm))
+                    value.forEach {
+                        it.removeFirst()
+                        if (it.isEmpty()) {
+                            it.add(Term("ε"))
+                        }
+                    }
+                    map[newTerm] = value
+                    entry.setValue(newValue)
+                }
+            }
+        }
+        map.forEach {
+            grammar.rules[it.key] = it.value
+        }
+        return grammar
+    }
+}
